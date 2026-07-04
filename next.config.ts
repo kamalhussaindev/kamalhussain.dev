e60135e98eae0099847607e884795d659a48eb4b
Kamal Hussain <74511326+kamalhussaindev@users.noreply.github.com>
2026-07-04 16:25:51 +0500

    Change output configuration from 'standalone' to 'export'

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import createMDX from '@next/mdx'
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  output: 'export',
   trailingSlash: true,
   turbopack: { root: __dirname },
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
